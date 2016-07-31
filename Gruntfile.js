e04834fd5490d934c7fbbfc33572e5236c9975b8
Paul Bergmeir <p.bergmeir@gmail.com>
2016-07-31 13:07:43 +0200

    Rewritten Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,111 +1,109 @@
-var srcPath = '_resources/';
-var scssSrcPath = srcPath + 'scss/';
-var scssDestPath = srcPath + 'css/';
-var cssSrcPath = scssDestPath;
-var cssDestPath = cssSrcPath;
-var jsSrcPath = srcPath + 'js/';
-var jsDestPath = jsSrcPath + 'min/';
+module.exports = function(grunt) {
 
+    var rootPath = '_resources/';
+    var cssPath = rootPath + 'css/';
+    var scssPath = rootPath + 'scss/';
+    var jsPath = rootPath + 'js/';
+    var imgPath = rootPath + 'img/';
 
-module.exports = function (grunt) {
+    // Create basic empty file
+    // var scssFile = scssPath + 'style.scss';
+    // var jsFile = jsPath + 'main.js';
+    // var content = '';
+
+    // grunt.file.write(scssFile, content);
+    // grunt.file.write(jsFile, content);
+
     grunt.initConfig({
-        watch: { // watch for file changes
-            sass: {
-                files: [
-                    scssSrcPath + 'style.scss'
-                ],
-                tasks: [
-                    'sass',
-                    'concat:css',
-                    'notify:watch'
-                ]
-            },
-            js: {
-                files: [
-                    jsSrcPath + '**.js'
-                ],
-                tasks: [
-                    // 'jshint',
-                    'uglify',
-                    'concat:js',
-                    'notify:watch'
-                ]
-            }
-        },
-
-        // jshint: { // check if js has errors
-        //     files: [
-        //         'Gruntfile.js',
-        //         jsSrcPath + '**.js'
-        //     ]
-        // },
-
-        uglify: { // minify js
-            src: {
-                src: [
-                    jsSrcPath + '**.js'
-                ],
-                dest: jsDestPath + 'main.min.js'
-            }
-        },
-
-        sass: { // compile scss to css
-            options: {
-                style: 'compressed'
-                // compress: true,
-                // sourceMap: 'auto'
-            },
-            src: {
-                src: [
-                    scssSrcPath + '**.scss'
-                ],
-                dest: scssDestPath + 'style.min.css'
-            }
-        },
-
-        concat: { // concatenate all js into one file and all css into another
-            options: {
-                separator: ';'
-            },
-            js: {
-                src: [
-                    jsSrcPath + 'jquery-2.1.1.min.js',
-                    jsSrcPath + 'bootstrap.min.js',
-                    jsSrcPath + '*.min.js'
-                ],
-                dest: jsDestPath + 'main.min.js'
-            },
-            css: {
-                src: [
-                    cssSrcPath + '*.min.css'
-                ],
-                dest: cssDestPath + 'style.min.css'
-            }
-        },
-
-        notify: { // send notification if compiling has finished
-            watch: {
+        mkdir: {
+            all: {
                 options: {
-                    message: 'compiled successful',
-                    success: true
+                    create: ['_resources/css', '_resources/scss', '_resources/js', '_resources/img']
                 }
             }
-        }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    '_resources/css/main.min.css': '_resources/scss/style.scss'
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    '_resources/js/min/main.min.js': [jsPath + '*.js'],
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', jsPath + '/*.js'],
+            options: {
+            // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        // imagemin: {
+        //     static: {
+        //         options: {
+        //             optimizationLevel: 3,
+        //             svgoPlugins: [{ removeViewBox: false }],
+        //             // use: [mozjpeg()]
+        //         },
+        //         files: {
+        //             '_resources/img/min/*.png': '_resources/img/*.png', // 'destination': 'source'
+        //             '_resources/img/min/*.jpg': '_resources/img/*.jpg',
+        //             '_resources/img/min/*.gif': '_resources/img/*.gif'
+        //         }
+        //     }
+        // },
+        notify: {
+            sass: {
+                options: {
+                    title: "Sass task complete",
+                    message: "CSS Files built"
+                }
+            },
+            js: {
+                options: {
+                    title: "JS task complete",
+                    message: "JS Files built"
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: [
+                    '_resources/scss/*.scss'
+                ],
+                tasks: ['sass', 'notify:sass']
+            },
+            js: {
+                files: [
+                    '_resources/js/*.js'
+                ],
+                tasks: ['uglify', 'notify:js']
+            }
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-notify');
-
-
-    grunt.registerTask('default', [
-        // 'jshint',
-        'uglify',
-        'sass',
-        'concat',
-        'notify:watch'
-    ]);
+    grunt.loadNpmTasks('grunt-mkdir');
+    // grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('init', ['mkdir']);
+    grunt.registerTask('default', ['sass', 'jshint', 'uglify', 'watch']);
 };
